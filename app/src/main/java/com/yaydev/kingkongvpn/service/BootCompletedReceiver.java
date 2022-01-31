ba1003f4763c5f3f16798bd9ca8fa32c4f2c454d
Your Name <you@example.com>
2022-01-31 17:40:33 +0700

    Start

---
+++ app/src/main/java/com/yaydev/kingkongvpn/service/BootCompletedReceiver.java
@@ -0,0 +1,38 @@
+package com.yaydev.kingkongvpn.service;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.yaydev.kingkongvpn.MainApplication;
+import com.yaydev.kingkongvpn.Preference;
+import com.yaydev.kingkongvpn.utils.Constant;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private int mJobId = 0;
+    Preference preference=new Preference(MainApplication.getContext());
+    private static final String TAG_BOOT_BROADCAST_RECEIVER = "BOOT_BROADCAST_RECEIVER";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ComponentName serviceComponent = new ComponentName(context, BackgroundJobService.class);
+        String action = intent.getAction();
+        String message = "BootDeviceReceiver onReceive, action is " + action;
+        Log.e(TAG_BOOT_BROADCAST_RECEIVER, action +" "+ preference.getStringpreference(Constant.SELECTED_COUNTRY));
+        if(Intent.ACTION_BOOT_COMPLETED.equals(action))
+        {
+
+            JobScheduler tm = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            if (tm != null) {
+                JobInfo.Builder builder = new JobInfo.Builder(mJobId++, serviceComponent);
+                builder.setMinimumLatency(0);
+                tm.schedule(builder.build());
+            }
+        }
+    }
+}
